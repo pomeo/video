b44bb8b576e48c7ef12cc6de172b91420a9e39c9
Sergey Ovechkin <me@pomeo.me>
2016-02-09 18:48:00 +0300

    countries list and categories list

---
+++ libs/category.js
@@ -0,0 +1,47 @@
+module.exports = {
+  Film: {
+    longName: 'Film & Animation',
+  },
+  Autos: {
+    longName: 'Autos & Vehicles',
+  },
+  Music: {
+    longName: 'Music',
+  },
+  Animals: {
+    longName: 'Pets & Animals',
+  },
+  Sports: {
+    longName: 'Sports',
+  },
+  Travel: {
+    longName: 'Travel & Events',
+  },
+  Games: {
+    longName: 'Gaming',
+  },
+  People: {
+    longName: 'People & Blogs',
+  },
+  Comedy: {
+    longName: 'Comedy',
+  },
+  Entertainment: {
+    longName: 'Entertainment',
+  },
+  News: {
+    longName: 'News & Politics',
+  },
+  Howto: {
+    longName: 'Howto & Style',
+  },
+  Education: {
+    longName: 'Education',
+  },
+  Tech: {
+    longName: 'Science & Technology',
+  },
+  Nonprofit: {
+    longName: 'Nonprofits & Activism',
+  },
+};
---
+++ libs/country.js
@@ -0,0 +1,122 @@
+module.exports = {
+  AR: {
+    en: 'Argentina',
+  },
+  AU: {
+    en: 'Australia',
+  },
+  AT: {
+    en: 'Austria',
+  },
+  BE: {
+    en: 'Belgium',
+  },
+  BR: {
+    en: 'Brazil',
+  },
+  CA: {
+    en: 'Canada',
+  },
+  CL: {
+    en: 'Chile',
+  },
+  CO: {
+    en: 'Colombia',
+  },
+  CZ: {
+    en: 'Czech Republic',
+  },
+  EG: {
+    en: 'Egypt',
+  },
+  FR: {
+    en: 'France',
+  },
+  DE: {
+    en: 'Germany',
+  },
+  GB: {
+    en: 'Great Britain',
+  },
+  HK: {
+    en: 'Hong Kong',
+  },
+  HU: {
+    en: 'Hungary',
+  },
+  IN: {
+    en: 'India',
+  },
+  IE: {
+    en: 'Ireland',
+  },
+  IL: {
+    en: 'Israel',
+  },
+  IT: {
+    en: 'Italy',
+  },
+  JP: {
+    en: 'Japan',
+  },
+  JO: {
+    en: 'Jordan',
+  },
+  MY: {
+    en: 'Malaysia',
+  },
+  MX: {
+    en: 'Mexico',
+  },
+  MA: {
+    en: 'Morocco',
+  },
+  NL: {
+    en: 'Netherlands',
+  },
+  NZ: {
+    en: 'New Zealand',
+  },
+  PE: {
+    en: 'Peru',
+  },
+  PH: {
+    en: 'Philippines',
+  },
+  PL: {
+    en: 'Poland',
+  },
+  RU: {
+    en: 'Russia',
+  },
+  SA: {
+    en: 'Saudi Arabia',
+  },
+  SG: {
+    en: 'Singapore',
+  },
+  ZA: {
+    en: 'South Africa',
+  },
+  KR: {
+    en: 'South Korea',
+  },
+  ES: {
+    en: 'Spain',
+  },
+  SE: {
+    en: 'Sweden',
+  },
+  CH: {
+    en: 'Switzerland',
+  },
+  TW: {
+    en: 'Taiwan',
+  },
+  AE: {
+    en: 'United Arab Emirates',
+  },
+  US: {
+    en: 'United States',
+  },
+};
